1da0274c5762d97716a7b58e17ac0587bbe7dd35
ptr-ito <ptr2839@gmail.com>
2022-12-13 22:13:43 +0900

    Add: サインアウトページを作成 #6

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,5 +1,95 @@
-import React from "react";
+import Cookies from "js-cookie";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { signUp } from "lib/api/auth";
+import { AuthContext } from "App";
+import { SignUpParams } from "interfaces/index";
 
 export const SignUp = () => {
-  return <div>サインアップページ</div>;
+  const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const confirmSuccessUrl = "http://localhost:3001/signin";
+
+  const handleSignUpSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const params: SignUpParams = {
+      name: name,
+      email: email,
+      password: password,
+      passwordConfirmation: passwordConfirmation,
+      confirmSuccessUrl: confirmSuccessUrl,
+    };
+
+    try {
+      const res = await signUp(params);
+      console.log(res);
+      alert("confirm email");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <>
+      <h1>サインアップページです</h1>
+      <form>
+        <div>
+          <label htmlFor="name">ユーザー名</label>
+          <input
+            type="name"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password_confirmation">パスワード確認</label>
+          <input
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="hidden"
+            id="confirm_success_url"
+            name="confirm_success_url"
+            value={confirmSuccessUrl}
+          />
+        </div>
+        <button type="submit" onClick={(e) => handleSignUpSubmit(e)}>
+          Submit
+        </button>
+      </form>
+      <Link to="/signin">サインインへ</Link>
+    </>
+  );
 };
