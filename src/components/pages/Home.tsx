7221737e93ec127df9bf92dc6f5603fe66788816
ptr-ito <ptr2839@gmail.com>
2022-12-13 22:12:28 +0900

    Add: サインイン後の動作検証用の記述を追加 #6

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "App";
 
 export const SampleHome = () => {
+  const { isSignedIn, currentUser } = useContext(AuthContext);
+
   return (
     <>
       <h1>Sample Home</h1>
+      {isSignedIn && currentUser ? (
+        <>
+          <h1>Signed in successfully!</h1>
+          <h2>Email: {currentUser?.email}</h2>
+          <h2>Name: {currentUser?.name}</h2>
+        </>
+      ) : (
+        <h1>Not signed in</h1>
+      )}
     </>
   );
 };
