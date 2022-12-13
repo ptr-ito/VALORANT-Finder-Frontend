b936e73b0691bd7408663ed3251cabf8d2e98c4f
ptr-ito <ptr2839@gmail.com>
2022-12-13 22:11:35 +0900

    Add: サインイン・サインアウト時の動作を追加 #6

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import Cookies from "js-cookie";
+
 import { AppBar, Box, Typography, Toolbar, Grid } from "@mui/material";
 import { css } from "@emotion/react";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Groups3Icon from "@mui/icons-material/Groups3";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
@@ -10,7 +12,72 @@ import HomeIcon from "@mui/icons-material/Home";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 
+import { signOut } from "lib/api/auth";
+import { AuthContext } from "App";
+
 const Header = () => {
+  const { loading, isSignedIn, setIsSignedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSignOut = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      const res = await signOut();
+
+      if (res.data.success === true) {
+        // サインアウト時には各Cookieを削除
+        Cookies.remove("_access_token");
+        Cookies.remove("_client");
+        Cookies.remove("_uid");
+
+        setIsSignedIn(false);
+        navigate("/");
+
+        console.log("Succeeded in sign out");
+      } else {
+        console.log("Failed in sign out");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const AuthButtons = () => {
+    // 認証完了後はサインアウト用のボタンを表示
+    // 未認証時は認証用のボタンを表示
+    if (!loading) {
+      if (isSignedIn) {
+        return (
+          <Button color="inherit" onClick={handleSignOut}>
+            ログアウト
+          </Button>
+        );
+      } else {
+        return (
+          <>
+            <Button
+              color="inherit"
+              component={Link}
+              to="/signin"
+              css={loginButton}
+            >
+              ログイン
+            </Button>
+            <Button
+              color="inherit"
+              component={Link}
+              to="/signup"
+              css={signupButton}
+            >
+              新規登録
+            </Button>
+          </>
+        );
+      }
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container>
@@ -62,15 +129,7 @@ const Header = () => {
                 <Typography css={contentMenuText}>マイページ</Typography>
               </Button>
             </Grid>
-            <Button color="inherit" css={loginButton}>
-              ログイン
-            </Button>
-            <Button color="inherit" css={signupButton}>
-              新規登録
-            </Button>
-            {/* <Button color="inherit" onClick={() => {}}>
-                ログアウト
-              </Button> */}
+            <AuthButtons />
           </Toolbar>
         </AppBar>
       </Grid>
