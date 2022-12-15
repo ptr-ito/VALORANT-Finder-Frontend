c662566645cc342bd0fcd5a13b59a41d21316d10
ptr-ito <ptr2839@gmail.com>
2022-12-15 21:58:13 +0900

    Remove: ログインユーザーの表示を削除 #6

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,21 +1,9 @@
 import React, { useContext } from "react";
-import { AuthContext } from "App";
 
 export const SampleHome = () => {
-  const { isSignedIn, currentUser } = useContext(AuthContext);
-
   return (
     <>
       <h1>Sample Home</h1>
-      {isSignedIn && currentUser ? (
-        <>
-          <h1>Signed in successfully!</h1>
-          <h2>Email: {currentUser?.email}</h2>
-          <h2>Name: {currentUser?.name}</h2>
-        </>
-      ) : (
-        <h1>Not signed in</h1>
-      )}
     </>
   );
 };
