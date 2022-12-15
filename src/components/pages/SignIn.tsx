1457be24c2a8acbf3ac5656ab8d2cd870c8ade2e
ptr-ito <ptr2839@gmail.com>
2022-12-15 21:58:22 +0900

    Add: サインインフォームにバリデーションの表示を追加 #6

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -4,6 +4,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { signIn } from "lib/api/auth";
 import { AuthContext } from "App";
 import { SignInParams } from "interfaces/index";
+import TextField from "@mui/material/TextField";
+import { Grid, Typography, Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { css } from "@emotion/react";
+import { signInSchema } from "validation/Schema";
 
 export const SignIn = () => {
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
@@ -11,11 +18,21 @@ export const SignIn = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  const [formError, setFormError] = useState("");
+
   const navigate = useNavigate();
 
-  const handleSignInSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm<SignInParams>({
+    mode: "onBlur",
+    resolver: zodResolver(signInSchema),
+  });
 
+  const signInSubmit: SubmitHandler<SignInParams> = async () => {
     const params: SignInParams = {
       email: email,
       password: password,
@@ -36,38 +53,95 @@ export const SignIn = () => {
         navigate("/");
       }
     } catch (e) {
+      setFormError("ユーザー名またはパスワードが正しくありません。");
       console.log(e);
     }
   };
   return (
     <>
-      <p>サインインページです</p>
-      <form>
-        <div>
-          <label htmlFor="email">メールアドレス</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">パスワード</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" onClick={(e) => handleSignInSubmit(e)}>
-          Submit
-        </button>
-      </form>
-      <Link to="/signup">サインアップへ</Link>
+      <Grid container direction="column" alignItems="center">
+        <Typography variant="h4" sx={{ mb: 8 }}>
+          ログイン
+        </Typography>
+        {formError}
+        <Box width={450}>
+          <form onSubmit={handleSubmit(signInSubmit)} noValidate>
+            <Typography>メールアドレス</Typography>
+            <TextField
+              variant="outlined"
+              sx={{ mb: "2rem" }}
+              required
+              fullWidth
+              placeholder="メールアドレスを入力してください"
+              value={email}
+              margin="dense"
+              {...register("email")}
+              error={!!errors["email"]}
+              helperText={errors.email ? errors.email?.message : ""}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <Typography>パスワード</Typography>
+            <TextField
+              variant="outlined"
+              sx={{ mb: "5rem" }}
+              required
+              fullWidth
+              type="password"
+              placeholder="パスワードを入力してください"
+              value={password}
+              margin="dense"
+              autoComplete="current-password"
+              {...register("password")}
+              error={!!errors["password"]}
+              helperText={errors.password ? errors.password?.message : ""}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              fullWidth
+              css={formLoginButton}
+            >
+              <Typography css={navText}>ログイン</Typography>
+            </Button>
+            <Box textAlign="center">
+              <Typography css={borderBottom}>
+                アカウントをお持ちでない場合は
+              </Typography>
+              <Button
+                variant="outlined"
+                color="inherit"
+                fullWidth
+                component={Link}
+                to="/signup"
+              >
+                新規登録する
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Grid>
     </>
   );
 };
+
+// css
+
+const formLoginButton = css`
+  background-color: #3f4551;
+  &:hover {
+    background-color: #3f4551;
+  }
+`;
+
+const navText = css`
+  font-weight: 550;
+`;
+
+const borderBottom = css`
+  border-top: 2px solid #ced1d8;
+  padding-top: 30px;
+  margin-top: 80px;
+  margin-bottom: 10px;
+`;
