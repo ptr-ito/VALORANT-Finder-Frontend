bdba9882404f48bab6f0ff68ff35fd9911f5db4e
ptr-ito <ptr2839@gmail.com>
2022-12-15 21:59:55 +0900

    Add: マイページへのRouteを追加 #6

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -2,10 +2,11 @@ import { memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import Post from "components/pages//post/Post";
 import { SampleHome } from "components/pages/Home";
-import NotFound from "components/pages/page404";
+import NotFound from "components/pages/Page404";
 import DefaultLayout from "components/layouts/DefaultLayout";
 import { SignUp } from "components/pages/SignUp";
 import { SignIn } from "components/pages/SignIn";
+import MyPage from "components/pages/MyPage";
 
 const Routers = memo(() => {
   return (
@@ -15,8 +16,9 @@ const Routers = memo(() => {
           <Route path="/" element={<SampleHome />} />
           <Route path="post" element={<Post />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="signup" element={<SignUp />}></Route>
-          <Route path="signin" element={<SignIn />}></Route>
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="mypage" element={<MyPage />} />
         </Route>
       </Routes>
     </>
