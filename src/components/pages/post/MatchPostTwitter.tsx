13b37e94b7d1df3f6e20872511b87c3371ff3c79
ptr-ito <ptr2839@gmail.com>
2023-02-07 21:20:49 +0900

    Add: twitterのシェアボタンをコンポーネントに切り分けた #75

---
+++ src/components/pages/post/MatchPostTwitter.tsx
@@ -0,0 +1,90 @@
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { TwitterShareButton } from "react-share";
+import { MatchPostTwitterProps } from "interfaces/index";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import MenuItem from "@mui/material/MenuItem";
+import { css } from "@emotion/react";
+
+const MatchPostTwitter = ({ matchPost, handleMenuClose }: MatchPostTwitterProps) => {
+  return (
+    <>
+      {matchPost?.attributes.mode === "コンペティティブ" && (
+        <TwitterShareButton
+          title={`${matchPost?.attributes.content}\n\nランク帯: ${matchPost?.attributes.rank}\n\n`}
+          url={`${import.meta.env.VITE_FRONT_URL}/post/${matchPost?.attributes.id}\n\n`}
+          hashtags={["VALORANT\n", "VALORANTコンペ募集\n", "VALORANTランク募集\n", "VALORANT募集"]}
+          onClick={handleMenuClose}
+        >
+          <MenuItem disableRipple={true}>
+            <ListItemIcon>
+              <TwitterIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText css={listTextColor} sx={{ ml: 3, mr: 3 }}>
+              募集内容をツイート
+            </ListItemText>
+          </MenuItem>
+        </TwitterShareButton>
+      )}
+      {matchPost?.attributes.mode === "アンレート" && (
+        <TwitterShareButton
+          title={`${matchPost?.attributes.content}\n\nランク帯: ${matchPost?.attributes.rank}\n\n`}
+          url={`${import.meta.env.VITE_FRONT_URL}/post/${matchPost?.attributes.id}\n\n`}
+          hashtags={["VALORANT\n", "VALORANTアンレート募集\n", "VALORANTアンレ募集\n", "VALORANT募集"]}
+          onClick={handleMenuClose}
+        >
+          <MenuItem disableRipple={true}>
+            <ListItemIcon>
+              <TwitterIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText css={listTextColor} sx={{ ml: 3, mr: 3 }}>
+              募集内容をツイート
+            </ListItemText>
+          </MenuItem>
+        </TwitterShareButton>
+      )}
+      {matchPost?.attributes.mode === "カスタム" && (
+        <TwitterShareButton
+          title={`${matchPost?.attributes.content}\n\nランク帯: ${matchPost?.attributes.rank}\n\n`}
+          url={`${import.meta.env.VITE_FRONT_URL}/post/${matchPost?.attributes.id}\n\n`}
+          hashtags={["VALORANT\n", "VALORANTカスタム募集\n", "VALORANT募集"]}
+          onClick={handleMenuClose}
+        >
+          <MenuItem disableRipple={true}>
+            <ListItemIcon>
+              <TwitterIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText css={listTextColor} sx={{ ml: 3, mr: 3 }}>
+              募集内容をツイート
+            </ListItemText>
+          </MenuItem>
+        </TwitterShareButton>
+      )}
+      {matchPost?.attributes.mode === "その他" && (
+        <TwitterShareButton
+          title={`${matchPost?.attributes.content}\n\nランク帯: ${matchPost?.attributes.rank}\n\n`}
+          url={`${import.meta.env.VITE_FRONT_URL}/post/${matchPost?.attributes.id}\n\n`}
+          hashtags={["VALORANT\n", "VALORANT募集"]}
+          onClick={handleMenuClose}
+        >
+          <MenuItem disableRipple={true}>
+            <ListItemIcon>
+              <TwitterIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText css={listTextColor} sx={{ ml: 3, mr: 3 }}>
+              募集内容をツイート
+            </ListItemText>
+          </MenuItem>
+        </TwitterShareButton>
+      )}
+    </>
+  );
+};
+
+export default MatchPostTwitter;
+
+// css
+
+const listTextColor = css`
+  color: #7f7f7f;
+`;
